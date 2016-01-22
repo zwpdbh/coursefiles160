58646ce6fdde1ad4b1972fee432b509df1401139
Hyperion <hyperion_z@outlook.com>
2016-01-23 11:40:20 +1300

    Lab16 Finished part3

--- Lab16/Average.java
+++ Lab16/Average.java
@@ -1,8 +1,15 @@
 public class Average{
   public static void main(String [] args){
-    int [] [] table = {{1,2,3},{4,5,6},{7,8}};
-  
-  
-  
+    int[][] table = {{1,2,3},{4,5,6},{7,8}};
+    
+    for (int[] rows: table) {
+       double sum = 0;
+      int count = 0;
+       for(int num: rows) {
+        System.out.print(num + " ");
+        sum += num;
+      }
+      System.out.format("\tAverage : %.1f \n", sum / rows.length);
+    }    
   }
 }
