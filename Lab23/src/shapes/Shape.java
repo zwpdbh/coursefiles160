e6dd0895a125bd36b00e8b461757817283b93329
Hyperion <hyperion_z@outlook.com>
2016-02-05 21:43:13 +1300

    >> make Lab23, make Shape become abstract <backup lab23 files, get ready

---
+++ Lab23/src/shapes/Shape.java
@@ -0,0 +1,61 @@
+package shapes;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Created by wzhao on 2/5/16.
+ */
+public class Shape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
+
+    private int moveX = 1;
+    private int moveY = 1;
+
+    public Shape() {
+        this.width = randomeRange(10, 30);
+        this.height = width;
+        this.x = randomeRange(0, 400 - width);
+        this.y = randomeRange(0, 400 - height);
+        this.color = new Color(randomeRange(0,255), randomeRange(0,255), randomeRange(0, 255));
+
+        // If shapes initially draw in the lower half of the screen, they travel upwards rather then downwards
+        if (y > 200) {
+            moveY = -1;
+        }
+    }
+
+    // (10) -> [0, 9],  20-30 -> (30-20)
+    public int randomeRange(int lo, int hi) {
+        Random generator = new Random();
+        return generator.nextInt(hi - lo + 1) + lo;
+    }
+
+    public void display(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, width, height);
+    }
+
+    public void move() {
+        // bounce back when meet boundary
+        if (y<=0 || y >= (400-height)) {
+            moveY = -moveY;
+        } else if (x<=0 || x >= (400-width)) {
+            moveX = -moveX;
+        }
+        // size bigger than 15, move up and down, smaller one move left and right
+        if (width >= 15) {
+            y += moveY;
+        } else {
+            x += moveX;
+        }
+    }
+
+    public String toString() {
+        return String.format("x: %d, y: %d, width: %d, height: %d", x, y, width, height);
+    }
+}
---
+++ Lab23/src/shapes/ShapePanel.java
@@ -0,0 +1,119 @@
+package shapes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
+
+/**
+ * Created by wzhao on 2/5/16.
+ */
+
+
+public class ShapePanel extends JPanel {
+    private Shape[] shapes;
+    private DrawingPanel drawingPanel;
+    private JPanel controlPanel;
+    private JButton addShape = new JButton("Add Shape");
+    private JTextField showNum;
+    private JLabel countLabel = new JLabel("Count");
+    private int count = 0;
+
+    private JButton start, stop;
+    private Timer timer;
+    private final int DELAY = 10;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lab Drawing");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        frame.getContentPane().add(new ShapePanel());
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    /**Constructor*/
+    public ShapePanel() {
+        shapes = new Shape[20];
+        JPanel controlPanel = new JPanel();     // control panel
+        addShape = new JButton("Add Shape");    // button
+        showNum = new JTextField(2);            // textField
+        countLabel = new JLabel("Count");       // label
+        drawingPanel = new DrawingPanel();      // the drawing panel
+        start = new JButton("Start");           // start button
+        stop = new JButton("Stop");             // stop button
+
+        controlPanel.add(addShape);             // add button, textField, label into the control panel
+        controlPanel.add(countLabel);
+        controlPanel.add(showNum);
+
+        controlPanel.add(start);
+        controlPanel.add(stop);
+        controlPanel.setPreferredSize(new Dimension(100, 400)); // set control panel's size
+
+        add(controlPanel);                      // add control panel and drawing panel into Shape panel
+        add(drawingPanel);
+
+        ButtonListener actionListener = new ButtonListener();
+        addShape.addActionListener(actionListener);
+        start.addActionListener(actionListener);
+        stop.addActionListener(actionListener);
+
+        // set timer
+        timer = new Timer(DELAY,actionListener);
+    }
+
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == addShape) {
+                if (count<20) {
+                    Shape item = new Shape();
+                    System.out.println(item);
+                    shapes[count] = item;
+                    count++;
+                } else {
+                    System.out.println("You reached 20 limit");
+                }
+            } else if (e.getSource() == stop) {
+                timer.stop();
+            } else {
+                timer.start();
+                try {
+                    for (Shape item: shapes) {
+                        item.move();
+                    }
+                } catch (NullPointerException event) {
+                    System.out.println("There is no object to move");
+                }
+            }
+
+            countLabel.setText(""+count);
+            repaint();
+        }
+    }
+
+    private class DrawingPanel extends JPanel {
+        public DrawingPanel() {
+            setPreferredSize(new Dimension(400, 400));
+            setBackground(Color.pink);
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            try {
+                for (int i=0; i<shapes.length; i++) {
+                    shapes[i].display(g);
+                }
+            } catch (NullPointerException e) {
+                System.out.println("There is no Shape object to draw");
+            }
+
+        }
+    }
+
+//    public Shape[] getShapes() {
+//        return shapes;
+//    }
+
+}
