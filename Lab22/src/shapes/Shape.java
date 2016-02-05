b499de6d30a3423612276caa8353aac4cd1c6d9d
Hyperion <hyperion_z@outlook.com>
2016-02-05 19:02:39 +1300

    >> start Lab 22 < copy files from lab 21, put them into lab22, then put them into a package named shapes

---
+++ Lab22/src/shapes/Shape.java
@@ -0,0 +1,38 @@
+package shapes;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Created by wzhao on 2/5/16.
+ */
+public class Shape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
+
+    public Shape() {
+        this.width = randomeRange(10, 30);
+        this.height = width;
+        this.x = randomeRange(0, 400 - width);
+        this.y = randomeRange(0, 400 - height);
+        this.color = new Color(randomeRange(0,255), randomeRange(0,255), randomeRange(0, 255));
+    }
+
+    // (10) -> [0, 9],  20-30 -> (30-20)
+    public int randomeRange(int lo, int hi) {
+        Random generator = new Random();
+        return generator.nextInt(hi - lo + 1) + lo;
+    }
+
+    public void display(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, width, height);
+    }
+
+    public String toString() {
+        return String.format("x: %d, y: %d, width: %d, height: %d", x, y, width, height);
+    }
+}
---
+++ Lab22/src/shapes/ShapePanel.java
@@ -0,0 +1,92 @@
+package shapes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
+
+/**
+ * Created by wzhao on 2/5/16.
+ */
+
+
+public class ShapePanel extends JPanel {
+    private Shape[] shapes;
+    private DrawingPanel drawingPanel;
+    private JPanel controlPanel;
+    private JButton addShape = new JButton("Add Shape");
+    private JTextField showNum;
+    private JLabel countLabel = new JLabel("Count");
+    private int count = 0;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lab Drawing");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        frame.getContentPane().add(new ShapePanel());
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    /**Constructor*/
+    public ShapePanel() {
+        shapes = new Shape[20];
+        JPanel controlPanel = new JPanel();     // control panel
+        addShape = new JButton("Add Shape");    // button
+        showNum = new JTextField(2);            // textField
+        countLabel = new JLabel("Count");       // label
+        drawingPanel = new DrawingPanel();      // the drawing panel
+
+        controlPanel.add(addShape);             // add button, textField, label into the control panel
+        controlPanel.add(countLabel);
+        controlPanel.add(showNum);
+        controlPanel.setPreferredSize(new Dimension(100, 400)); // set control panel's size
+
+        add(controlPanel);                      // add control panel and drawing panel into Shape panel
+        add(drawingPanel);
+
+        addShape.addActionListener(new ButtonListener());
+    }
+
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == addShape) {
+                if (count<20) {
+                    Shape item = new Shape();
+                    System.out.println(item);
+                    shapes[count] = item;
+                    count++;
+                } else {
+                    System.out.println("You reached 20 limit");
+                }
+            }
+            countLabel.setText(""+count);
+            repaint();
+        }
+    }
+
+    private class DrawingPanel extends JPanel {
+        public DrawingPanel() {
+            setPreferredSize(new Dimension(400, 400));
+            setBackground(Color.pink);
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            try {
+                for (int i=0; i<shapes.length; i++) {
+                    shapes[i].display(g);
+                }
+            } catch (NullPointerException e) {
+                System.out.println("There is no Shape object to draw");
+            }
+
+        }
+    }
+
+//    public Shape[] getShapes() {
+//        return shapes;
+//    }
+
+}
